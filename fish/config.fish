2503088e6c1327874e59af3789bc5699d202f83e
AmitaWhite <amitawhite@AmitaWhites-MacBook-Pro-13.local>
2024-08-04 00:46:11 +0900

    [fix] 각 로컬 환경 home에서 하드링크 생성으로 정책 변경

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,14 @@
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-#eval /Users/amitawhite/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
+# ANDROID Setting
+set -x ANDROID_HOME /Users/amitawhite/Library/Android/sdk
+set -x PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
+set -x PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin
+set -x PATH $PATH $HOME/.sdkman/bin
+
+
+# JAVA Setting
+set -x JAVA_HOME_11 /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
+set -x JAVA_HOME_14 /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+set -x JAVA_HOME_17 /Users/amitawhite/Library/Java/JavaVirtualMachines/graalvm-ce-17/Contents/Home
+
+set -x JAVA_HOME $JAVA_HOME_17
