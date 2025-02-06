a795e178c8b3ae16fee7d1c5e2cb7adc5956b9f9
Amita <amita@Amitas-MacBook-Air.local>
2025-02-06 19:20:34 +0900

    [air edit]tmp

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,15 @@
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-#eval /Users/amitawhite/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f /usr/local/Caskroom/miniconda/base/bin/conda
+    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/usr/local/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/usr/local/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/usr/local/Caskroom/miniconda/base/bin" $PATH
+    end
+end
 # <<< conda initialize <<<
    alias desktophide '~/desktophide.sh'
 set -x PATH $PATH /usr/local
