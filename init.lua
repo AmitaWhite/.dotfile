f58bfdc63c2730585ff2d619d1e226bde81a6f0e
Amita <amitawhite75@gmail.com>
2025-11-11 00:55:41 +0900

    I can't find my dotfiles error!!

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
+-- Disable deprecation warnings temporarily
+vim.deprecate = function() end
+-- init.lua 또는 관련 설정 파일에 추가
 require("Amita.core")
 require("Amita.lazy")
--- lua/Amita/plugins/lsp/mason.lua
+++ lua/Amita/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				--"tsserver",
+				"tsserver",
 				"ts_ls",
 				"html",
 				"cssls",
@@ -39,6 +39,8 @@ return {
 				"prismals",
 				"pyright",
 				"clangd",
+				"gql",
+				"emmet_ls",
 			},
 		})
 
