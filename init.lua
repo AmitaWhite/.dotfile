cfe70c198a26cc137c7550d033c7c111a92699b6
Amita <amita@arch.net>
2025-11-14 06:05:04 +0900

    solve : mason_lspconfig 핸들러 오류 - 디렉토리 중첩 기재 때문이었음 + vim 경로 추가

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
+vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
 -- Disable deprecation warnings temporarily
 vim.deprecate = function() end
 -- init.lua 또는 관련 설정 파일에 추가
