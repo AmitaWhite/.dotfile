a9406dbecef6819497e415888159dcc47aa42fb1
amita <amita@wsl-arch.com>
2025-11-14 06:13:35 +0900

    Same solve - arch-linux (mason lsp set_up handler)

--- lua/Amita/lazy.lua
+++ lua/Amita/lazy.lua
@@ -12,7 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
-	{ { import = "Amita.plugins", cond = not vim.g.vscode }, { import = "Amita.plugins.lsp", cond = not vim.g.vscode } },
+	{ { import = "Amita.plugins", cond = not vim.g.vscode } },
 	{
 		checker = {
 			enabled = true,
