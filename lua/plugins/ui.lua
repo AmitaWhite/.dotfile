3b94c2f7c9b46b28918cc022c41f354d05144301
AmitaWhite <amitawhite@AmitaWhites-MacBook-Pro-13.local>
2026-04-28 15:05:08 +0900

    feat : pane 구분 plugin 추가

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,12 +1,5 @@
 return {
-  {
-    "m00qek/baleia.nvim",
-    version = "*",
-    config = function()
-      vim.g.baleia = require("baleia").setup({
-        line_starts_at = 1,
-        log_level = "error",
-      })
-    end,
-  },
+  "nvim-zh/colorful-winsep.nvim",
+  config = true,
+  event = { "WinLeave" },
 }
